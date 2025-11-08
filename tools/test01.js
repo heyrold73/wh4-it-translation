aa1ec7d5eb18f48a41a81fdef572f4d45c24428c
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2025-11-08 15:06:02 +0100

    Delete tools directory

--- tools/test01.js
+++
@@ -1,6 +0,0 @@
-let item1 = await fromUuid("Compendium.wfrp4e-core.items.Item.EO05HX7jql0g605A")
-let item2 = await fromUuid("Compendium.wfrp4e-core.items.Item.Bvd2aZ0gQUXHfCTh")
-let ids = await this.actor.createEmbeddedDocuments("Item", [item1, item2], {fromEffect : this.effect.id})
-console.log(ids)
-this.actor.updateEmbeddedDocuments('Item', [ {id: ids[0], 'system.specification.value': 16} ] )
-
