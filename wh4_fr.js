66ad6761a99f4dea1a6a335b0ce94fe9c3f58a1c
heyrold73 <63519965+heyrold73@users.noreply.github.com>
2026-02-03 12:51:18 +0100

    new release

--- wh4_fr.js
+++ wh4_fr.js
@@ -1,4 +1,4 @@
-Hooks.once("init", function() {
-	CONFIG.wfrp4e.supportedLanguages["fr"] = "fr";
-	CONFIG.eis.supportedLanguages["fr"] = "fr";
-})
+Hooks.once("init", function () {
+  CONFIG.wfrp4e.supportedLanguages["it"] = "it";
+  CONFIG.eis.supportedLanguages["it"] = "it";
+});
